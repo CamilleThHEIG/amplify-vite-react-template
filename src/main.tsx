278f357e081132b6f7898dc17a99d249ac1b2c44
CamilleThHEIG <camille.theubet@hes-so.ch>
2025-06-09 17:26:09 +0200

    bing

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
 // auth
-//import { Authenticator } from '@aws-amplify/ui-react';
+// import { Authenticator } from '@aws-amplify/ui-react';
 // <Authenticator><App/></Authenticator>
 import '@aws-amplify/ui-react/styles.css';
 
